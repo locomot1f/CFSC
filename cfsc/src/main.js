32b9f933f192263a84b34d3402a478e8b3c77278
benjamin f. gunter iv <ben@pop-os.localdomain>
2020-04-23 19:27:28 -0400

    trying to get draggable to work on index.html

--- cfsc/src/main.js
+++ cfsc/src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Draggable } from "@shopify/draggable";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  Draggable,
   render: h => h(App)
 }).$mount("#app");
